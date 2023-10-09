7f59fad3963059a18b398634240fed4470214996
Kim Min Kyoung <minkyoung2550@gmail.com>
2023-10-09 15:03:31 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "First app",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First app'),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('Hello'),
+            Text('Hello'),
+            Text('hello'),
+          ],
+        ),
+      ),
+    );
+  }
+}
